193c1a19c394f73ef55fd7e40427eadce23150d9
steef <94022938+steefst33f@users.noreply.github.com>
2023-10-18 19:41:38 +0200

    Moved dispenser code to own file

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,10 @@
 #include "Nfc.h"
 #include "UriComponents.h"
 #include "Payment.h"
+#include "Dispenser.h"
 
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-
-#include <ESP32Servo.h>
-
-//GPIO
-int servoPin = 27;
-int vendorModePin = 33;
-int fillDispencerButton = 0;
-int emptyDispencerButton = 35;
 
 String ssidPrefix = "LNVending";
 String ssid = ssidPrefix + String(ESP.getEfuseMac(), HEX);  // Unique SSID based on a prefix and the ESP32's chip ID
@@ -23,7 +16,6 @@ WiFiConfiguration wifiSetup(ssid.c_str(), password.c_str());
 Payment payment = Payment();
 
 Nfc nfc = Nfc();
-Servo servo;
 
 void displayWifiSetup(String ssid, String password, String ip);
 // String scannedLnUrlFromNfcTag();
@@ -223,34 +215,6 @@ void onWiFiEvent(WiFiEvent_t event) {
       displayScreen("Wifi", message);
       break;
   }
-}
-
-/// Servo Code
-//////// Servo ///////
-void dispense() {
-  Serial.println(F("Vending Machine dispense START!"));
-  servo.writeMicroseconds(1000); // rotate clockwise (from the buyers point of view)
-  delay(1920);
-  servo.writeMicroseconds(1500);  // stop
-  Serial.println(F("Vending Machine dispense STOP!!"));
-}
-
-void fillDispenser() {
-  Serial.println(F("Fill dispenser!!"));
-  // let dispencer slowly turn in the fillup direction, so the vender can empty the dispendser with new products:
-  servo.writeMicroseconds(2000); // rotate counter clockwise (from the buyers point of view)
-  delay(1590);
-  servo.writeMicroseconds(1500);  // stop
-  Serial.println(F("Done!"));
-}
-
-void emptyDispenser() {
-  Serial.println(F("Empty dispenser!!"));
-  // let dispencer slowly turn in dispence direction, so the vender can empty all products from dispenser:
-  servo.writeMicroseconds(1000); // rotate clockwise (from the buyers point of view)
-  delay(1920);
-  servo.writeMicroseconds(1500);  // stop
-  Serial.println(F("Done!"));
 }
 
 //NFC callback handlers
